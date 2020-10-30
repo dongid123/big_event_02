5a5432a2e4c092748e3539f6704dcabbb4311b25
dong <dong@itcast.cn>
2020-10-30 19:08:10 +0800

    个人中心完成

--- assets/js/user/user.avatar.js
+++ assets/js/user/user.avatar.js
@@ -1,5 +1,5 @@
 $(function () {
-
+    var layer = layui.layer
     // 1.1 获取裁剪区域的 DOM 元素
     var $image = $('#image')
     // 1.2 配置选项
@@ -12,4 +12,56 @@ $(function () {
 
     // 1.3 创建裁剪区域
     $image.cropper(options)
+
+    // 点击上传按钮触发上传文件事件
+    $("#btnAdd").on('click', function () {
+        $("#file").click()
+    })
+
+    // 触发事件上传头像
+    $("#file").on('change', function (e) {
+        // 1.拿到用户选择的文件
+        var fileList = e.target.files[0]
+        if (fileList == undefined) {
+            return layer.msg('请您上传图片')
+        }
+        // 2.根据选择的文件，创建一个对应的 URL 地址：
+        var newImgURL = URL.createObjectURL(fileList)
+        // 3. 先销毁旧的裁剪区域，再重新设置图片路径，之后再创建新的裁剪区域：
+        $image
+            .cropper('destroy')      // 销毁旧的裁剪区域
+            .attr('src', newImgURL)  // 重新设置图片路径
+            .cropper(options)        // 重新初始化裁剪区域
+    })
+
+
+    // 头像上传
+    $('#btnAddImags').on('click', function () {
+        // 将裁剪后的图片，输出为 base64 格式的字符串
+        var dataURL = $image
+            .cropper('getCroppedCanvas',
+                { // 创建一个 Canvas 画布
+                    width: 100,
+                    height: 100
+                })
+            .toDataURL('image/png')
+        // 将 Canvas 画布上的内容，转化为 base64 格式的字符串
+        $.ajax({
+            type: 'POST',
+            url: '/my/update/avatar',
+            data: {
+                avatar: dataURL
+            },
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg(res.message)
+                }
+                // 上传完成
+                layer.msg("恭喜您头像更新完成");
+                window.parent.gitUserInof()
+            }
+        })
+    })
+
+
 })
